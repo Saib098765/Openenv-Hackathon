c4d8f193477f491c9b58bcf10fb73e4cb46e2607
saisbala <sai.sleghana.b.bala@oracle.com>
2026-04-08 01:10:41 +0530

    final

--- server/app.py
+++ server/app.py
@@ -26,7 +26,6 @@ def main(host: str = "0.0.0.0", port: int = 7860):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--host", default="0.0.0.0")
     parser.add_argument("--port", type=int, default=7860)
     args = parser.parse_args()
-    main(host=args.host, port=args.port)
+    main(port=args.port)

5c65c8e1d3d9861fa23a33680c7e1b02e60b03a3
saisbala <sai.sleghana.b.bala@oracle.com>
2026-04-08 01:28:10 +0530

    final

--- server/app.py
+++ server/app.py
@@ -19,13 +19,16 @@ app = create_app(
     max_concurrent_envs=1, 
 )
 
-def main(host: str = "0.0.0.0", port: int = 7860):
+def main():
     import uvicorn
-    uvicorn.run(app, host=host, port=port)
-
-if __name__ == '__main__':
     import argparse
+    
     parser = argparse.ArgumentParser()
+    parser.add_argument("--host", default="0.0.0.0")
     parser.add_argument("--port", type=int, default=7860)
     args = parser.parse_args()
-    main(port=args.port)
+    
+    uvicorn.run(app, host=args.host, port=args.port)
+
+if __name__ == "__main__":
+    main()
